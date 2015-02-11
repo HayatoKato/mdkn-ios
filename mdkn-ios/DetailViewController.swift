9990534c926eea4ca7e73c4994f0e21c18fd9208
hayato kato <h.kato345@gmail.com>
2015-02-12 01:36:12 +0900

    Add git ignore

---
+++ mdkn-ios/DetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.swift
+//  mdkn-ios
+//
+//  Created by 加藤羽也人 on 2015/02/12.
+//  Copyright (c) 2015年 Hayato Kato. All rights reserved.
+//
+
+import Cocoa
+
+class DetailViewController: UIViewController {
+
+}
